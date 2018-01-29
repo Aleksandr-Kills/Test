2a399a0f3d10e750d535852434d158e459ac0dda
JAY <+>
2018-01-29 20:46:05 +0200

    content-commit

--- app/js/common.js
+++ app/js/common.js
@@ -1,3 +1,14 @@
+// burger-menu
 $('.main-item').click(function(){
     $('.nav').fadeToggle();
+});
+
+// slider
+jQuery('.slider').slick ({
+    dots: false,
+    arrows:false,
+    autoplay: true,
+    speed: 1000,
+    draggable: false,
+    fade:true
 });
